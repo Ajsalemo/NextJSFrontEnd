459b0d0f60a9593e19fe1e9206e68f4722f784c9
Anthony Salemo <ajssalemo@gmail.com>
2020-10-11 20:18:00 -0400

    added a stylized block navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,32 @@
+import { Fragment } from "react";
+import Link from "next/link";
+
+const Navbar = () => (
+  <Fragment>
+    <nav className="inset-x-0 top-0 h-16 flex">
+      <div className="bg-black w-1/4 h-24 relative"></div>
+      <div className="bg-gray-700 w-1/4 h-24 relative"></div>
+      <div className="bg-gray-500 w-1/4 h-24 relative"></div>
+      <div className="bg-gray-700 w-1/4 h-24 relative"></div>
+      <div className="bg-black w-1/4 h-24 relative"></div>
+    </nav>
+    {/* Styling used for the navbar blocks */}
+    <style jsx>{`
+      div:before {
+        content: "";
+        position: absolute;
+        background: black;
+        top: 0;
+        left: -25px;
+        bottom: 0;
+        width: 50px;
+        -webkit-transform: skew(-20deg);
+        -moz-transform: skew(-20deg);
+        -ms-transform: skew(-20deg);
+        transform: skew(-20deg);
+      }
+    `}</style>
+  </Fragment>
+);
+
+export default Navbar;
